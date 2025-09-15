228d1451000d88d51bd31eb0d68445edbe532f84
SamuelLJG <samuelljg@outlook.com>
2025-09-15 20:16:32 -0300

    teste

--- next.config.ts
+++ next.config.ts
@@ -33,7 +33,10 @@ const nextConfig: NextConfig = {
         permanent: false,
       }
     ];
-  }
+  },
+  output: "export", // para gerar site estático
+  basePath: "/genshin-nextjs", // substitua pelo nome do seu repo
+  assetPrefix: "/genshin-nextjs/", // garante que assets e imagens funcionem
 };
 
 export default nextConfig;
