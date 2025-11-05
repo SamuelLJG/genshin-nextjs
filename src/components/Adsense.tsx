a0594adb333c8369879997a40621ccab4067927a
SamuelLJG <samuelljg@outlook.com>
2025-11-05 14:48:12 -0300

    teste2

--- src/components/Adsense.tsx
+++ src/components/Adsense.tsx
@@ -1,44 +1,35 @@
+// Exemplo: src/components/AdSlot.tsx
+
 "use client";
 
 import Script from "next/script";
-import { useEffect } from "react";
 
-export default function GptAd() {
-  useEffect(() => {
-    // Garante que o objeto googletag existe
-    window.googletag = window.googletag || { cmd: [] };
-
-    window.googletag.cmd.push(function () {
-      const slotId = "div-gpt-ad-1606164528140-0";
-
-      // Evita recriar o slot se já existir
-      if (!window.googletag.pubads().getSlots().some(s => s.getSlotElementId() === slotId)) {
-        window.googletag
-          .defineSlot(
-            "/22971553453/genshinbuild.com/genshinbuild.com_mw_content1_05112025",
-            [[250, 250], [336, 280], [300, 250]],
-            slotId
-          )
-          .addService(window.googletag.pubads());
-        window.googletag.enableServices();
-      }
-
-      // Exibe o anúncio
-      window.googletag.display(slotId);
-    });
-  }, []);
-
+export default function AdSlot() {
   return (
     <>
-      {/* Carrega a biblioteca GPT de forma assíncrona */}
-      <Script
-        id="gpt-lib"
-        strategy="afterInteractive"
-        src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
-      />
+      <Script id="gpt-init" strategy="afterInteractive">
+        {`
+          window.googletag = window.googletag || {cmd: []};
+          googletag.cmd.push(function() {
+            googletag.defineSlot(
+              "/22971553453/genshinbuild.com/genshinbuild.com_mw_content1_05112025",
+              [[250, 250], [336, 280], [300, 250]],
+              "div-gpt-ad-1606164528140-0"
+            ).addService(googletag.pubads());
+            googletag.enableServices();
+          });
+        `}
+      </Script>
 
-      {/* Div onde o anúncio será renderizado */}
-      <div id="div-gpt-ad-1606164528140-0" style={{ textAlign: "center", margin: "1rem 0" }} />
+      <div id="div-gpt-ad-1606164528140-0">
+        <Script id="gpt-display" strategy="afterInteractive">
+          {`
+            googletag.cmd.push(function() { 
+              googletag.display("div-gpt-ad-1606164528140-0"); 
+            });
+          `}
+        </Script>
+      </div>
     </>
   );
 }
