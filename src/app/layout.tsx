87ff835092978f602ddc338681da7d9c2c0689c9
SamuelLJG <samuelljg@outlook.com>
2025-11-06 19:59:29 -0300

    test interstitial 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default async function RootLayout({ children }: {children: React.ReactNod
             window.googletag = window.googletag || {cmd: []};
         var bloco_interstitial;
         googletag.cmd.push(function() {        
-        bloco_interstitial = googletag.defineOutOfPageSlot('/22106840220,22971553453/genshinbuild.com/genshinbuild.com_Interstitial_11082023', googletag.enums.OutOfPageFormat.INTERSTITIAL);
+        bloco_interstitial = googletag.defineOutOfPageSlot('/22971553453/genshinbuild.com/genshinbuild.com_Interstitial_05112025', googletag.enums.OutOfPageFormat.INTERSTITIAL);
         if (bloco_interstitial) bloco_interstitial.addService(googletag.pubads());
         googletag.enableServices();
         googletag.display(bloco_interstitial);
