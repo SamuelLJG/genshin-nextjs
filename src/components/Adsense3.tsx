28dd8649eb7e260494759f855d6e4e754d597b87
SamuelLJG <samuelljg@outlook.com>
2026-01-15 15:04:02 -0300

    removed inpage ads

--- src/components/Adsense3.tsx
+++ src/components/Adsense3.tsx
@@ -2,25 +2,5 @@
 import Script from "next/script"
 
 export default function AdSense() {
-return (
-<div className="adContainer">
-
-<ins
-className="adsbygoogle"
-style={{display:'block'}}
-data-ad-client="ca-pub-1999593447203691"
-data-ad-slot="9925646389"
-data-ad-format="auto"
-data-full-width-responsive="true"
-/>
-
-
-<Script
-id={`ads-init-9925646389`}
-dangerouslySetInnerHTML={{
-__html: `(adsbygoogle = window.adsbygoogle || []).push({});`,
-}}
-/>
-</div>
-);
+return '';
 }
