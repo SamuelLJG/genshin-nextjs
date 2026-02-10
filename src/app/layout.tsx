b44bd7d2500199d7975fab5ae241f7b3de61010c
SamuelLJG <samuelljg@outlook.com>
2026-02-09 22:51:36 -0300

    test htaccess

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,6 @@ import { Anuphan, Plus_Jakarta_Sans, Inter, Oswald } from 'next/font/google'
 import "./globals.css";
 import { Metadata } from "next";
 import { GoogleAnalytics } from '@next/third-parties/google'
-import AdTargetingRaw from '@/components/AdTargetingRaw';
-import Script from 'next/script';
 export const metadata: Metadata = {
   metadataBase: new URL('https://genshinbuild.com'),
   icons: {
